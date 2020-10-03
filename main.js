6dbb0b921650de3a1755d8a4f6b5bc22043b5035
Caner Caliskaner <caner.caliskaner@cognite.com>
2020-10-03 17:11:30 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var TurndownService = require('turndown');
+var axios = require('axios');
+const cheerio = require('cheerio');
+
+function main(reddit) {
+    var turndownService = new TurndownService()
+
+    reddit = reddit.replace('www.reddit.com', 'old.reddit.com')
+    axios.get(reddit, {
+        headers: {
+            Referer: reddit,
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    }).then(function (response) {
+        const $ = cheerio.load(response.data)
+        var md = cheerio.html($(".md")[1]);
+        console.log(turndownService.turndown(md));
+    });
+}
+
+main('https://old.reddit.com/r/i3wm/comments/j4ej8t/setting_combo_of_multiple_modifiers_as_mod/')
